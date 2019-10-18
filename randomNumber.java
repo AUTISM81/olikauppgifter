4044517d1b8b313d10636e0a2a703d73f54e5095
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2019-10-18 10:54:09 +0200

    random number, lektion 18/10-19 KLAR

--- randomNumber.java
+++ randomNumber.java
@@ -3,24 +3,32 @@ import javax.swing.*;
 public class randomNumber {
     public static void main (String [] args){
         //variabler
-        int Number = 46;
+        int Number;
         int guess1;
         int amount = 1;
+
+        //Random nummer
+
+        Number = (int)Math.floor(Math.random() * 101);
 
         //kod
 
         String guess = JOptionPane.showInputDialog(null, "Please enter a number between 1 and 100");
         guess1 = Integer.parseInt(guess);
 
+
         while ( Number != guess1) {
-            if (guess1 < 46)
+            if (guess1 < Number)
                 JOptionPane.showMessageDialog(null, "your guess is too low!");
-            if (guess1 > 46)
+            if (guess1 > Number)
                 JOptionPane.showMessageDialog(null, "your guess is too high!");
             guess = JOptionPane.showInputDialog(null, "Please enter a number between 1 and 100");
             guess1 = Integer.parseInt(guess);
             amount = amount + 1;
         }
-        JOptionPane.showMessageDialog(null, "your guess was correct, it took you " +  amount + " guesses!");
+        if (amount == 1)
+            JOptionPane.showMessageDialog(null, "WOW! It took you only one guess!" + "\nCongrats");
+        if (amount > 1)
+            JOptionPane.showMessageDialog(null, "your guess was correct, it took you " +  amount + " guesses!" + "\nNOOB");
     }
 }
